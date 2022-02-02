e703692d156df634b32326a87d716ead9ff97f18
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:31 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'portSet':    {
 			label: 'Choose port',
